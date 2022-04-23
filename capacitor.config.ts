df3b0344aac7b9eb735fe04aa182f0f7909441f7
Kencse Szabolcs <szabolcskencse@gmail.com>
2022-04-23 16:53:03 +0200

    added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'usochool',
+  webDir: 'dist/usochool',
+  bundledWebRuntime: false
+};
+
+export default config;
