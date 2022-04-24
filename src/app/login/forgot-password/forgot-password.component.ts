593a634f09145c87428e271665e4c184669a3eac
Kencse Szabolcs <szabolcskencse@gmail.com>
2022-04-24 17:08:44 +0200

    forgot password popup created

--- src/app/login/forgot-password/forgot-password.component.ts
+++ src/app/login/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public matDialogRef: MatDialogRef<ForgotPasswordComponent>,
+  ) { }
 
   ngOnInit(): void {
   }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { take } from 'rxjs/operators';
 
@@ -33,7 +33,13 @@ export class LoginComponent implements OnInit {
   }
 
   async forgotPassword() {
-    const dialog = this.matDialog.open(ForgotPasswordComponent);
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = true;
+    dialogConfig.panelClass = 'forgot-password';
+    dialogConfig.backdropClass = 'forgot-password-backdrop';
+    dialogConfig.maxWidth = '100vw';
+
+    const dialog = this.matDialog.open(ForgotPasswordComponent,dialogConfig);
     await dialog.afterClosed().pipe(take(1)).toPromise();
     console.log(`closed`);
     
